44a904f9e16e97ef2e63a1550edfa95ff3746a9b
CJLawson-design <lawsoncj3@gmail.com>
2023-02-17 00:13:04 -0600

    added button styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ price }) => {
   return (
-    <button className="flex flex-row justify-center items-center bg-[#222222] rounded-full text-slate-50 w-full pt-4 pb-4">
+    <button className="flex flex-row justify-center items-center bg-[#222222] rounded-full text-slate-50 w-full pt-4 pb-4 hover:opacity-95">
       <div className="mr-4"></div>
       <div>
         <h1>Add to Cart · {price}</h1>
