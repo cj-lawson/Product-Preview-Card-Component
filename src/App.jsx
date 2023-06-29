e43a333b53b505d4521e201269c5ab84016375a6
CJLawson-design <lawsoncj3@gmail.com>
2023-06-29 12:21:02 -0500

    updated copy

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,6 @@ function App() {
       </div>
       <footer className="pb-2">
         <div className="flex flex-col justify-between">
-          <div className="mb-[2px]">
-            <p className="text-xs">
-              Challenge by{" "}
-              <span>
-                <a
-                  href="https://www.frontendmentor.io?ref=challenge"
-                  className="text-emerald-700 underline"
-                >
-                  Frontend Mentor
-                </a>
-              </span>
-              . Designed and coded by
-              <span>
-                {" "}
-                <a
-                  href="https://github.com/spicyBeefPho"
-                  className="text-emerald-700 underline"
-                >
-                  CJ Lawson
-                </a>
-              </span>
-              .
-            </p>
-          </div>
           <div>
             <p className="text-xs text-center">
               Photos and copy from
